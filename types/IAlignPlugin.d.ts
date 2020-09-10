03133130966ead8906e63a10d846c0c0de352c92
liurongqing <phpgege@gmail.com>
2020-09-10 16:10:16 +0800

    add ts type

--- types/IAlignPlugin.d.ts
+++ types/IAlignPlugin.d.ts
@@ -1,16 +1,19 @@
 declare module 'phaser3-align-plugin' {
-  class AlignPlugin{}
+  class AlignPlugin { }
 }
+
+
 
 declare namespace Phaser {
   export interface Scene {
     align: {
-      vw: Function
-      vh: Function
-      grid: Function
-      placeAt: Function
-      placeAtIndex: Function
-      envelop: Function
+      envelop(obj: any, scale: number): void
+      vw(obj: any, scale: number): void
+      vh(obj:any, scale: number): void
+      grid(obj: {rows: number, cols: number, color: any, debug: boolean}): void
+      placeAt(row: number, col: number, obj: any, origin: number[]): void
+      placeAtIndex(index: number, obj: any, origin: number[]): void
+      
     }
   }
 }
